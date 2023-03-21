46c2de4e5d1d61a175ca4cf472680b9ce44c4b24
NiravRVaghasiya <nirav.r.vaghasiya@gmail.com>
2023-03-21 20:59:39 +0100

    Python script updated to prepare dictionary in descanding oreder of the values i.e. according number of files per label and csv file is created

--- analyzedataset.py
+++ analyzedataset.py
@@ -1,8 +1,10 @@
 import os
 import re
+import csv 
+
+
 audiofiles=0
 count = 0
-
 labels=[]
 for root_dir, cur_dir, files in os.walk(r'dataset/all-samples/'):
     count += len(files)
@@ -12,10 +14,16 @@ for root_dir, cur_dir, files in os.walk(r'dataset/all-samples/'):
                 name=re.split('_', str(file))
                 labels.append(name[0])
 print('file count:', audiofiles)
-for label in set(labels):
-     number=labels.count(label)
-     print(f'{label}, {number}')
-print(set(labels))
-print(len(set(labels)))
-#for label in set(labels):
- #    print(f'{label}, number')
+
+my_dict = dict(map(lambda x: (x, labels.count(x)), labels))
+sorted_dict = dict(sorted(my_dict.items(), key=lambda x: x[1], reverse=True))
+with open('analyzed_data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Class', 'Number of files'])
+    for key, value in sorted_dict.items():
+        writer.writerow([key, value])
+
+with open('analyzed_data.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
